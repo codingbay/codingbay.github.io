278cded1db3512eace3857a601e076fc37b21858
Tuan Ho <2703372+calibay@users.noreply.github.com>
2021-03-13 21:23:25 -0800

    update

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ let canvasLeft = canvas.offsetLeft;
 let canvasTop = canvas.offsetTop;
 const ctx = canvas.getContext('2d');
 const pos = { x: 0, y: 0 };
+const colorPalette = [
+  'black',
+  'white',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'indigo',
+  'violet',
+  'pink',
+  'brown',
+  'grey',
+];
+const brushSizes = [5, 10, 20];
+
+let brushColor = colorPalette[0];
+let brushSize = brushSizes[0];
 
 resize();
 
@@ -28,12 +46,44 @@ function setPosition(event) {
 function draw(event) {
   if (event.buttons !== 1) return false;
   ctx.beginPath();
-  ctx.lineWidth = 5;
+  ctx.lineWidth = brushSize;
   ctx.lineCap = 'round';
-  ctx.strokeStyle = 'red';
+  ctx.strokeStyle = brushColor;
   ctx.moveTo(pos.x, pos.y);
   setPosition(event);
   ctx.lineTo(pos.x, pos.y);
   ctx.closePath();
   ctx.stroke();
 }
+
+const palette = document.getElementById('palette');
+colorPalette.forEach(function (color) {
+  const newColorDiv = document.createElement('div');
+  newColorDiv.style.backgroundColor = color;
+  newColorDiv.className = 'option-container';
+  newColorDiv.addEventListener('click', function () {
+    brushColor = color;
+  });
+  palette.appendChild(newColorDiv);
+});
+
+const newEraserDiv = document.createElement('div');
+newEraserDiv.className = 'option-container';
+newEraserDiv.addEventListener('click', function () {
+  brushColor = colorPalette[1];
+});
+const eraser = document.createElement('div');
+eraser.className = 'eraser';
+newEraserDiv.appendChild(eraser);
+palette.appendChild(newEraserDiv);
+
+brushSizes.forEach(function (size) {
+  const newBrushDiv = document.createElement('div');
+  newBrushDiv.backgroundColor = 'white';
+  newBrushDiv.className = 'option-container';
+  newBrushDiv.style.background = `radial-gradient(${size}px circle at 50% 50%, black, white)`;
+  newBrushDiv.addEventListener('click', function () {
+    brushSize = size;
+  });
+  palette.appendChild(newBrushDiv);
+});
